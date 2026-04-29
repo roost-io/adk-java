cfee3421fc4bd225f096e147db13b9dd151448c7
roost-io <8110509+mgdevstack@users.noreply.github.com>
2026-04-29 11:01:18 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4.1

---
+++ core/src/test/java/com/google/adk/models/Categories.java
@@ -0,0 +1,33 @@
+package com.google.adk.models;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface generateContent {
+
+	}
+
+	interface generateContent {
+
+	}
+
+	interface convertAnthropicResponseToLlmResponse {
+
+	}
+
+}
